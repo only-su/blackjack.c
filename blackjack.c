7f771a526d8383f525dd458dce1a280e2a334292
Arthur Caccavo <arthurcaccavo45@gmail.com>
2018-11-18 18:32:28 -0200

    jogo individual finalizado

--- blackjack.c
+++ blackjack.c
@@ -109,25 +109,26 @@ void imprime_mao(JOGADOR *jg, char ident)
 {
 	int i;
 	char scarta[5];
-	char lines [5][100];
+	char lines [5][5000];
 
-	for (i = 0; i < 5; i++) lines[i][0] = '\0';
+	for (i = 0; i < 5; i++) strcpy(lines[i], " ");
 
 	if (ident == 'm') {
-		sprintf(lines[0], "┌─────┐");
-		sprintf(lines[1], "│░░░░░│");
-		sprintf(lines[2], "│░░░░░│");
-		sprintf(lines[3], "│░░░░░│");
-		sprintf(lines[4], "└─────┘");
+		sprintf(lines[0], " ┌─────┐");
+		sprintf(lines[1], " │░░░░░│");
+		sprintf(lines[2], " │░░░░░│");
+		sprintf(lines[3], " │░░░░░│");
+		sprintf(lines[4], " └─────┘");
 	}
 
 	for (i = (ident == 'm') ? 1 : 0; i < jg->qtd_mao; i++) {
 		traduz_valor(jg->mao[i].valor, scarta);
 		sprintf(lines[0], "%s┌─────┐", lines[0]);
 		sprintf(lines[1], "%s│%s    │", lines[1], jg->mao[i].naipe);
-		sprintf(lines[2], "%s│  %s%s │", lines[2],
-			scarta,
-			(strcmp(scarta, "10")) ? " " : "");
+		sprintf(lines[2], "%s│ %c%s%c │", lines[2],
+			(strcmp(scarta, "10")) ? ' ' : scarta[0],
+			(!strcmp(scarta, "10")) ? " " : scarta,
+			(strcmp(scarta, "10")) ? ' ' : scarta[1]);
 		sprintf(lines[3], "%s│    %s│", lines[3], jg->mao[i].naipe);
 		sprintf(lines[4], "%s└─────┘", lines[4]);
 	}
@@ -218,41 +219,32 @@ void titulo()
 int main(void)
 {
 	int pts, pts_mesa, aposta, rodada;
+	int dft_montante = 200;
 	char c, ident;
 	BARALHO bar;
-	JOGADOR jg, mesa;
+	JOGADOR jg;
+	JOGADOR mesa;
+	char msg[50];
+	char input[100];
 
 	/* O jogo inicia com um menu tendo como opções começar o jogo ou sair do
 	 * programa */
 	while (1) {
 		titulo();
-		printf("Digite 1 para começar o jogo\n");
-		printf("Digite 2 para configurar\n");
+		printf("Digite e para começar o jogo (¢%d)\n", dft_montante);
+		printf("Digite c para configurar\n");
 		printf("Digite qualquer outra tecla para sair\n");
 		c = getchar();
-		while ('\n' != getchar())
-			; /* Limpa a entrada */
-		if (c == '1') {
-			/* O jogador deve digitar um quantidade de dinheiro com a
-			 * a qual iniciará o jogo. O programa repete até que um valor
-			 * válido seja digitado */
-			while (1) {
-				titulo();
-				printf("Digite um montante inicial para você:\n");
-				if (!scanf("%d", &jg.dinheiro) || jg.dinheiro <= 0) {
-					while ('\n' != getchar())
-						; /* Limpa a entrada */
-					printf("Este valor não é válido para a aposta!\n");
-				} else {
-					break;
-				}
-			}
+		while ('\n' != getchar());      /* Limpa a entrada */
+		if (c == 'e') {
+			rodada = 1;             /* O programa começa a contar as rodadas */
+			jg.dinheiro = dft_montante;
 
-			rodada = 1; /* O programa começa a contar as rodadas */
 			while (1) {
 				bar = iniciar_baralho();
 				embaralhar(&bar);
 				ident = 'm';
+				strcpy(msg, "");
 
 				/* O programa pede um valor para ser apostado nessa rodada.
 				 * Se o jogador ganhar a rodada ele ganha o dobro da aposta, se perder ele
@@ -262,15 +254,17 @@ int main(void)
 					titulo();
 					printf("Rodada %d\n", rodada);
 					printf("Montante atual: ¢%d\n", jg.dinheiro);
-					printf("\nDigite um valor para a aposta dessa rodada:\n");
+					printf("%sDigite um valor para a aposta dessa rodada:\n¢", msg);
 					if (!scanf("%d", &aposta) || aposta > jg.dinheiro || aposta <= 0) {
-						while ('\n' != getchar())
-							; /* Limpa a entrada */
-						printf("Este valor não é válido para a aposta!\n");
+						while ('\n' != getchar());      /* Limpa a entrada */
+						sprintf(msg, "Este valor não é válido para a aposta! ");
 					} else {
 						break;
 					}
 				}
+
+				strcpy(msg, "");
+
 				while ('\n' != getchar());      /* Limpa a entrada */
 				jg.dinheiro -= aposta;          /* Remove a aposta do montante do jogador */
 
@@ -283,54 +277,67 @@ int main(void)
 				while (1) {
 					/* Mostra a mao do jogador e da mesa. Uma das cartas da mesa fica
 					 * virada. */
-					if (ident == 'm') titulo();
+					titulo();
 					printf("Rodada %d\n", rodada);
+					printf("Montante atual: ¢%d\n", jg.dinheiro);
 					printf("Aposta atual: ¢%d\n", aposta);
+
 					printf("\nMão da mesa:\n");
 					imprime_mao(&mesa, ident);
+					pts_mesa = pontuaMao(mesa.mao, mesa.qtd_mao);
+					printf("\n Pontuação atual: %d\n", (ident != 'm') ? pts_mesa : pts_mesa - pontuaMao(mesa.mao, 1));
+
 					printf("\nSua mão:\n");
 					imprime_mao(&jg, 'j');
+					pts = pontuaMao(jg.mao, jg.qtd_mao);
+					printf("\n Pontuação atual: %d\n", pts);
+
+					printf("%s", msg);
 
 					if (ident == 'j') break;
-
-					pts = pontuaMao(jg.mao, jg.qtd_mao);
-					printf("\n\nPontuação atual: %d\n", pts);
 
 					/* Se a pontuacao do jogar for igual a 21 ele automaticamente
 					 * ganha, se for maior que 21 ele automaticamente perde */
 					if (pts > 21) {
-						titulo();
-						printf("\nA pontuação estourou!\nVocê perdeu!\n");
+						sprintf(msg, "\nA pontuação estourou!\n");
 						ident = 'j';
 					} else if (pts == 21) {
-						titulo();
 						if (jg.qtd_mao == 2 &&
-						    ((jg.mao[0].valor == 10 && jg.mao[1].valor == 1) ||
-						     (jg.mao[0].valor == 1 && jg.mao[1].valor == 10)))
-							printf("\nBlackJack! Você ganhou!");
+						    ((jg.mao[0].valor >= 9 && jg.mao[1].valor == 0) ||
+						     (jg.mao[0].valor == 0 && jg.mao[1].valor >= 9)))
+							sprintf(msg, "\nBlackJack! Alguém vai jantar frango essa noite!");
 						else
-							printf("\nA pontuação deu 21!\nVocê ganhou!\n");
+							sprintf(msg, "\nA pontuação deu 21!\n");
 						ident = 'j';
 					}
 
 					while (ident == 'm') {
-						printf("\nDigite 1 para puxar outra carta\n");
-						printf("Digite 2 para abaixar a mao\n"); /* TODO mostrar a mão da Mesa
-						                                          * no final */
+						printf("\nDigite s para finalizar a rodada (stand)\n");
+						printf("%s", (c == 'd') ? "" : "Digite h para pedir mais cartas (hit)\n");
+						printf("%s", (c == 'd' || (aposta) > jg.dinheiro) ? "" : "Digite d para dobrar a aposta (double down)\n");
+						printf("Digite 0 para desistir (surrender)\n");
 						c = getchar();
-						while ('\n' != getchar())
-							; /* Limpa a entrada */
-						if (c == '1') {
+						while ('\n' != getchar()); /* Limpa a entrada */
+						if (c == 'h' || c == 'd') {
 							jg.mao[jg.qtd_mao++] = puxaCarta(&bar);
+							if (c == 'd') {
+								jg.dinheiro -= aposta;
+								aposta += aposta;
+								sprintf(msg, "\nVocê dobrou sua aposta! Agora é tudo ou nada!!!\n");
+							}
 							break;
-						} else if (c == '2') {
+						} else if (c == 's') {
+							break;
+						} else if (c == '0') {
 							break;
 						}
 					}
-					if (c == '2') {
-						pts_mesa = pontuaMao(mesa.mao, mesa.qtd_mao);
-						while (pts_mesa < pts)
+
+					if (c == 's' || c == '0') {
+						while (pts_mesa < 17 && c != '0') {
 							mesa.mao[mesa.qtd_mao++] = puxaCarta(&bar);
+							pts_mesa = pontuaMao(mesa.mao, mesa.qtd_mao);
+						}
 						ident = 'j';
 						titulo();
 					}
@@ -338,18 +345,28 @@ int main(void)
 
 				pts_mesa = pontuaMao(mesa.mao, mesa.qtd_mao);
 
-				if (pts > pts_mesa) {
-					jg.dinheiro += aposta * 2;
-				} else if (pts < pts_mesa) {
+				if (c == '0') {
+					printf("\nVocê desistiu...");
+					jg.dinheiro += aposta / 2;
+				} else if ((pts > pts_mesa && pts <= 21) || pts_mesa > 21) {
+					printf("\nParabéns você ganhou essa rodada!!");
+					jg.dinheiro += (jg.qtd_mao == 2 &&
+							((jg.mao[0].valor >= 9 && jg.mao[1].valor == 0) ||
+							 (jg.mao[0].valor == 0 && jg.mao[1].valor >= 9))) ? (aposta * 2) + (aposta / 2) : aposta * 2;
+				} else if (pts == pts_mesa) {
+					printf("\nEmpate!");
+					jg.dinheiro += aposta;
+				} else {
+					printf("\nQue pena a mesa ganhou dessa vez 😢😢");
 				}
 
 				while (1) {
-					printf("\nMontante resultante:%d\n", jg.dinheiro);
+					printf("\nMontante resultante: ¢%d\n\n", jg.dinheiro);
 					if (jg.dinheiro == 0) {
-						printf("Parece que você faliu 😢😢\n Você perdeu!");
-						c = 'n';
+						printf("Parece que você faliu 😢😢\n");
+						c = 's';
 					} else {
-						printf("Digite c para continuar ou s para sair:\n");
+						printf("Digite c para continuar ou s para sair: ");
 						c = getchar();
 						while ('\n' != getchar())
 							; /* Limpa a entrada */
@@ -361,13 +378,26 @@ int main(void)
 						break;
 					}
 				}
-				// system("clear");
-				/*TODO printar o resultado final, adicionar um sistema de score*/
-				if (c == 's')
+				if (c == 's') {
+					printf("Digite qualquer coisa para voltar ao menu principal...");
+					getchar();
+					break;
+				}
+			}
+		} else if (c == 'c') {
+			/* TODO menu para mudar o montante padrão e a quantidade de jogadores */
+			/* O jogador deve digitar um quantidade de dinheiro com a
+			 * a qual iniciará o jogo. O programa repete até que um valor
+			 * válido seja digitado */
+			while (1) {
+				titulo();
+				printf("Digite um montante padrão para as partidas:\n");
+				fgets(input, 100, stdin);
+				if (!sscanf(input, "%d", &dft_montante) || dft_montante <= 0)
+					printf("Este valor não é válido!\n");
+				else
 					break;
 			}
-		} else if (c == '2') {
-			/* code */
 		} else {
 			break;
 		}
